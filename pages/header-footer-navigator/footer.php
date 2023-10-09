26dc9fb44062857fc45d8a31ed0327b9fe2c45b7
Jerry <tiennennakenki@gmail.com>
2023-10-09 20:02:52 +0700

    Divide header-footer-navigator to 3 file

---
+++ pages/header-footer-navigator/footer.php
@@ -0,0 +1,232 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- Tải các tệp CSS của Bootstrap từ CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Tải các tệp JavaScript của Bootstrap từ CDN (đặt nó trước đó trước thẻ đóng </body>) -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
+    <!-- FILE CSS -->
+    <link rel="stylesheet" type="text/css" href="css/header-footer-navigator.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+<hr>
+    <footer>
+        <div class="Row">
+            <div class="FooterTop">
+                <div class="BoxCenter">
+                    <div class="BoxFooter1">
+                        <h5>Hỗ trợ Khách hàng</h5>
+                        <a href="https://help.phongvu.vn/dich-vu-khach-hang/the-thanh-vien" target="_blank"
+                            rel="noreferrer noopener" class="CssColorBlack css-no-underline">Thẻ ưu đãi
+                        </a>
+                        <a href="https://help.phongvu.vn/chinh-sach-ban-hang/huong-dan-mua-hang-online" target="_blank"
+                            rel="noreferrer noopener" class="CssColorBlack css-no-underline">Hướng dẫn mua online
+                        </a>
+                        <a href="https://help.phongvu.vn/dich-vu-khach-hang/doanh-nghiep-than-thiet" target="_blank"
+                            rel="noreferrer noopener" class="CssColorBlack css-no-underline">Ưu đãi dành cho Doanh
+                            nghiệp
+                        </a>
+                        <a href="https://help.phongvu.vn/chinh-sach-ban-hang/tra-gop" target="_blank"
+                            rel="noreferrer noopener" class="CssColorBlack css-no-underline">Chính sách trả góp
+                        </a>
+                        <a href="https://help.phongvu.vn/dich-vu-khach-hang/dich-vu-sua-chua-va-bao-tri" target="_blank"
+                            rel="noreferrer noopener" class="CssColorBlack css-no-underline">Dịch vụ sửa chữa
+                        </a>
+                    </div>
+                    <div class="BoxFooter2">
+                        <h5>Chính sách mua hàng</h5>
+                        <a href="https://help.phongvu.vn/chinh-sach-ban-hang/dieu-kien-giao-dich" target="_blank"
+                            rel="noreferrer noopener" class="CssColorBlack css-no-underline">Điều kiện giao dịch chung
+                        </a>
+                        <a href="https://help.phongvu.vn/chinh-sach-ban-hang/bao-hanh" target="_blank"
+                            rel="noreferrer noopener" class="CssColorBlack css-no-underline">Chính sách bảo hành
+                        </a>
+                        <a href="https://help.phongvu.vn/chinh-sach-ban-hang/doi-tra-va-hoan-tien" target="_blank"
+                            rel="noreferrer noopener" class="CssColorBlack css-no-underline">Chính sách đổi trả
+                        </a>
+                        <a href="https://help.phongvu.vn/chinh-sach-ban-hang/chinh-sach-thanh-toan"
+                            class="CssColorBlack css-no-underline">Chính sách thanh toán
+                        </a>
+                        <a href="https://help.phongvu.vn/chinh-sach-ban-hang/giao-hang-va-lap-dat-tai-nha"
+                            target="_blank" rel="noreferrer noopener" class="CssColorBlack css-no-underline">Giao hàng
+                            và Lắp đặt tại
+                            nhà
+                        </a>
+                        <a href="https://help.phongvu.vn/chinh-sach-ban-hang/dich-vu-lap-dat-va-nang-cap-tai-cua-hang"
+                            target="_blank" rel="noreferrer noopener" class="CssColorBlack css-no-underline">Dịch vụ lắp
+                            đặt và nâng cấp
+                            PC/ Laptop tại cửa hàng &amp; TTBH
+                        </a>
+                    </div>
+                    <div class="BoxFooter3">
+                        <h5>Thông tin Phong Vũ</h5>
+                        <a href="https://help.phongvu.vn/" target="_blank" rel="noreferrer noopener"
+                            class="CssColorBlack css-no-underline">Giới thiệu Phong Vũ
+                        </a>
+                        <a href="https://phongvu.vn/p/he-thong-showroom-phong-vu" target="_blank"
+                            rel="noreferrer noopener" class="CssColorBlack css-no-underline">Hệ thống cửa hàng
+                        </a>
+                        <a href="https://help.phongvu.vn/dich-vu-khach-hang/trung-tam-bao-hanh" target="_blank"
+                            rel="noreferrer noopener" class="CssColorBlack css-no-underline">Trung tâm bảo hành
+                        </a>
+                        <a href="https://help.phongvu.vn/chinh-sach-chung/bao-mat-thong-tin" target="_blank"
+                            rel="noreferrer noopener" class="CssColorBlack css-no-underline">Chính sách bảo mật
+                        </a>
+                        <a href="https://phongvu.vn/cong-nghe" target="_blank" rel="noreferrer noopener"
+                            class="CssColorBlack css-no-underline">Tin công nghệ
+                        </a>
+                        <a href="https://phongvu.vn/hoi-dap" target="_blank" rel="noreferrer noopener"
+                            class="CssColorBlack css-no-underline">Hỏi đáp
+                        </a>
+                        <a href="https://tuyendung.phongvu.vn/" target="_blank" rel="noreferrer noopener"
+                            class="CssColorBlack css-no-underline">Tuyển dụng
+                        </a>
+                    </div>
+                    <div class="BoxFooter4">
+                        <h5>Cộng đồng Phong Vũ</h5>
+                        <div class="CssColorBlackDiv ">Gọi mua hàng (miễn phí)
+                            <a class="css-no-underline" style="color:blue" href="tel:18006867" }="">18006867
+
+                            </a>
+                        </div>
+                        <div class="CssColorBlackDiv">Gọi chăm sóc
+                            <a class="css-no-underline" style="color:blue" href="tel:18006865" }="">18006865
+                            </a>
+                        </div>
+                        <a href="https://www.facebook.com/phongvuvietnam/" target="_blank" rel="noreferrer noopener"
+                            class="CssColorBlack css-no-underline"><span size="16" color="#485992"
+                                class="css-zmasuu"></span>
+                            <div class="CssColorBlackDiv" style="color:blue; display:inline-block; margin-right: 8px"><i
+                                    class="fa-brands fa-facebook"></i>
+                            </div>Facebook Phong Vũ
+                        </a>
+                        <a href="https://www.youtube.com/channel/UCOJ3sqWCc6koR9BScBZkBlA" target="_blank"
+                            rel="noreferrer noopener" class="CssColorBlack css-no-underline">
+                            <span size="16" color="#cf2200" class="css-btiuvm">
+                            </span>
+                            <div class="CssColorBlackDiv" style="color:red; display:inline-block; margin-right: 8px">
+                                <i class="fa-brands fa-youtube"></i>
+                            </div>Phong Vũ Media
+                        </a>
+                        <a href="https://www.facebook.com/groups/phongvuvn/" target="_blank" rel="noreferrer noopener"
+                            class="CssColorBlack css-no-underline">
+                            <div class="CssColorBlackDiv" style="color:green; display:inline-block; margin-right: 8px">
+                                <i class="fa-solid fa-people-group"></i>
+                            </div>Phong Vũ Hội
+                        </a>
+                        <a href="https://zalo.me/3951230446867820356" target="_blank" rel="noreferrer noopener"
+                            class="CssColorBlack css-no-underline">
+                            <div class="CssColorBlackDiv" style="color:blue; display:inline-block; margin-right: 8px">
+                                <i class="fa-brands fa-android"></i>
+                            </div>
+                            OA Phong Vũ (zalo)
+                        </a>
+                    </div>
+                    <div class="BoxFooter5">
+                        <h5>Email liên hệ</h5>
+                        <div class="CssColorBlackDiv">Hỗ trợ Khách hàng:<br>
+                            <a class="css-no-underline" style="color:blue" href="mailto:cskh@phongvu.vn">cskh@phongvu.vn
+                            </a>
+                        </div>
+                        <div class="CssColorBlackDiv">Liên hệ báo giá:<br>
+                            <a class="css-no-underline" style="color:blue"
+                                href="mailto:baogia@phongvu.vn">baogia@phongvu.vn
+                            </a>
+                        </div>
+                        <div class="CssColorBlackDiv">Hợp tác phát triển:<br>
+                            <a class="css-no-underline" style="color:blue"
+                                href="mailto:hoptac@phongvu.vn">hoptac@phongvu.vn
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="FooterBottom">
+                <div class="BoxCenter">
+                    <div class="BoxFooterLeft">
+                        <div class="BoxFooterLeft1">
+                            <h5>Phương thức thanh toán</h5>
+                        </div>
+                        <div class="BoxFooterLeft2">
+                            <div class="Payment clickable">
+                                <i class="fa-solid fa-qrcode Icon"></i>
+                                <a class="css-no-underline HoverBlue" style="font-size: 12px;" href="">QR Code</a>
+                            </div>
+                            <div class="Payment clickable">
+                                <i class="fa-solid fa-money-bill-1 Icon"></i>
+                                <a class="css-no-underline HoverBlue" style="font-size: 12px;" href="">Tiền mặt</a>
+                            </div>
+                            <div class="Payment clickable">
+                                <i class="fa-regular fa-clock Icon"></i>
+                                <a class="css-no-underline HoverBlue" style="font-size: 12px;" href="">Trả góp</a>
+                            </div>
+                            <div class="Payment clickable">
+                                <i class="fa-brands fa-internet-explorer Icon" style="text-align: center"></i>
+                                <a class="css-no-underline HoverBlue" style="font-size: 12px;" href="">Internet
+                                    Banking</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="BoxFooterRight">
+                        <div class="BoxFooterRight1">
+                            <h5>Danh sách các ngân hàng thanh toán online</h5>
+                            <img src="https://shopfront-cdn.tekoapis.com/static/vnpay_banks.png" alt="" width="70%">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="Row FooterBottomParent" style="background-color: rgb(233, 237, 242);">
+            <div class="FooterBottom2">
+                <div class="FooterBottomLeff ">
+                    <div class="FooterTop">Công ty cổ phần thương mại - dịch vụ Phong Vũ</div>
+                    <div class="FooterBottom">© 1997 - 2020 Công Ty Cổ Phần Thương Mại - Dịch Vụ Phong Vũ<br>Giấy chứng
+                        nhận đăng ký doanh nghiệp: 0304998358 do Sở KH-ĐT TP.HCM cấp lần đầu ngày 30 tháng 05 năm 2007
+                    </div>
+                </div>
+                <div class="FooterBottomCenter">
+                    <div>
+                        <span class="Title">Địa chỉ trụ sở chính</span>:
+                        <div class="Content">Tầng 5, Số
+                            117-119-121 Nguyễn Du, Phường Bến Thành, Quận 1, Thành Phố Hồ Chí Minh
+                        </div>
+                    </div>
+                    <div>
+                        <span class="Title">Văn phòng điều hành miền Bắc</span>:
+                        <div class="Content">Tầng 6, Số 1
+                            Phố Thái Hà, Phường Trung Liệt, Quận Đống Đa, Hà Nội
+                        </div>
+                    </div>
+                    <div>
+                        <span class="Title">Văn phòng điều hành miền Nam</span>:
+                        <div class="Content">677/2A Điện
+                            Biên Phủ, Phường 25 , Quận Bình Thạnh , TP. Hồ Chí Minh
+                        </div>
+                    </div>
+                </div>
+                <div class="FooterBottomRight">
+                    <a href="http://online.gov.vn/Home/WebDetails/4549" rel="noreferrer noopener" target="_blank"
+                        class="A">
+                        <img src="https://shopfront-cdn.tekoapis.com/common/da-dang-ky.png" alt="verified logo"
+                            class="css-t5rvn4">
+                    </a>
+                    <div class="Div">
+                        <a href="//www.dmca.com/Protection/Status.aspx?ID=53b44883-ed2a-434d-902b-5adce10aafd5&amp;refurl=undefined"
+                            title="DMCA.com Protection Status" class="dmca-badge">
+                            <img src="https://images.dmca.com/Badges/dmca-badge-w100-2x1-02.png?ID=53b44883-ed2a-434d-902b-5adce10aafd5"
+                                alt="DMCA.com Protection Status">
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
