f1b6dd9f20405f02f32801ad4cbeb4df14d2cab6
trungkienmvp <86387886+trungkienmvp@users.noreply.github.com>
2023-10-22 17:16:25 +0700

    bootstrap_add

--- index.php
+++ index.php
@@ -12,7 +12,9 @@
     <link rel="stylesheet" href="css/home-page.css">
     <link rel="stylesheet" href="send-mail/style_send-mail.css">
     <link rel="stylesheet" href="css/login-page.css">
-
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <!-- Tải các tệp CSS của Bootstrap từ CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
